cd0a47b641cc4b1850e3e115455a68cab8b51706
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2023-08-07 21:31:23 +0530

    Update pattern .cpp

--- Pattern/pattern .cpp
+++ Pattern/pattern .cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
   int n = 5;
-  
-    for(int i = 1; i <= n; i++)
-   {
-      for(int j = 1; j <= i; j++)
+    for(int i = 1; i <= n; i++){
+         for(int j = 1; j <= i; j++)
           cout << "* ";
-             cout << endl;
+            cout << endl;
    } 
-  return 0;
+return 0;
 }
